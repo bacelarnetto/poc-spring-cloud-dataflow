af06426e51e09a58761d5c4b0c8e91c5ef31c277
bacelarnetto <bacelar.netto.analista@gmail.com>
2021-03-28 13:18:29 -0300

    first commit

---
+++ batchs/project/spring-batch-demo-one/src/main/java/br/com/demo/one/SpringBatchDemoOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemoOneApplication.class, args);
+	}
+
+}
---
+++ batchs/project/spring-batch-demo-one/src/test/java/br/com/demo/one/SpringBatchDemoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
