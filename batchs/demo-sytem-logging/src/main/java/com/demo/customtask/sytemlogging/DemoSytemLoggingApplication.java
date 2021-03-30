921b6e97a6e1c46f79f79d521642671378e6e4ef
bacelarnetto <bacelar.netto.analista@gmail.com>
2021-03-30 19:39:24 -0300

    melhorias

--- batchs/demo-sytem-logging/src/main/java/com/demo/customtask/sytemlogging/DemoSytemLoggingApplication.java
+++ batchs/demo-sytem-logging/src/main/java/com/demo/customtask/sytemlogging/DemoSytemLoggingApplication.java
@@ -1,32 +1,15 @@
 package com.demo.customtask.sytemlogging;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
-import org.springframework.context.annotation.Bean;
 
 @EnableTask
 @SpringBootApplication
-public class DemoSytemLoggingApplication {
-	
-	@Bean
-	public MySytemLog mySytemLog() {
-		return new MySytemLog();
-	}
+public class DemoSytemLoggingApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSytemLoggingApplication.class, args);
-	}
-	
-
-	public static class MySytemLog implements ApplicationRunner{
-		@Override
-		public void run(ApplicationArguments args) throws Exception {
-			System.out.println(String.format("Olá, execultando a task Sytem Logging!!"));
-			
-		}		
 	}
 
 }
